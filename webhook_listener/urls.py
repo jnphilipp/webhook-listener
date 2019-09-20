02133feb906e24adcd493e1973a83f656f0cf288
J. Nathanael Philipp <nathanael@philipp.land>
2019-09-20 14:06:11 +0200

    Add License.

--- webhook_listener/urls.py
+++ webhook_listener/urls.py
@@ -1,5 +1,20 @@
 # -*- coding: utf-8 -*-
 # Copyright (C) 2019 J. Nathanael Philipp (jnphilipp) <nathanael@philipp.land>
+#
+# This file is part of webhook_listener.
+#
+# webhook_listener is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# webhook_listener is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with webhook_listener. If not, see <http://www.gnu.org/licenses/>.
 """webhook_listener URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
@@ -15,6 +30,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import re_path
 
